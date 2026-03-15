f45bfa3a792b97b00f68e48c86ecde5cf8b9930e
volkangurus55 <volkanguru52@hotmail.com>
2026-03-16 00:16:19 +0300

    Fix database connection timeout

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-15T21:15:49.481Z
-Commit: Add export functionality for reports
+Timestamp: 2026-03-15T21:16:19.631Z
+Commit: Fix database connection timeout
