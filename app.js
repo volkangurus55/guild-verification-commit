f879a362c7b84542f5c9a17fba18a6b6d49a776d
volkangurus55 <volkanguru52@hotmail.com>
2026-03-16 00:15:08 +0300

    Resolve null pointer exception in user service

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-15T21:14:54.078Z
-Commit: Add multi-language support
+Timestamp: 2026-03-15T21:15:08.384Z
+Commit: Resolve null pointer exception in user service
