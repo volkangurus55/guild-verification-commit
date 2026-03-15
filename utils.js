181882ca85614ccd85c6b5d66a50cd8bf5c34b1a
volkangurus55 <volkanguru52@hotmail.com>
2026-03-16 00:15:14 +0300

    Resolve null pointer exception in user service

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-15T21:15:10.532Z
-Commit: Correct typo in configuration file
+Timestamp: 2026-03-15T21:15:14.641Z
+Commit: Resolve null pointer exception in user service
