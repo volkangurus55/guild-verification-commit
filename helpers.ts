7e836cb786b1045999e16996e2e7f4015f63ee15
volkangurus55 <volkanguru52@hotmail.com>
2026-03-16 00:15:15 +0300

    Fix memory leak in event handlers

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-15T21:15:02.746Z
-Commit: Introduce real-time notifications
+Timestamp: 2026-03-15T21:15:15.643Z
+Commit: Fix memory leak in event handlers
