d8555957ed33856330c0b939c8a16a31e91ae8f6
volkangurus55 <volkanguru52@hotmail.com>
2026-03-16 00:15:19 +0300

    Fix broken links in documentation

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-15T21:14:59.471Z
-Commit: Add export functionality for reports
+Timestamp: 2026-03-15T21:15:19.075Z
+Commit: Fix broken links in documentation
