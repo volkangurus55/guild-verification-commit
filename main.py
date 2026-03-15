cd33cb5c6058172fcbf0b05a2ba15b365dae92a0
volkangurus55 <volkanguru52@hotmail.com>
2026-03-16 00:15:25 +0300

    Patch API endpoint error handling

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-15T21:15:19.075Z
-Commit: Fix broken links in documentation
+Timestamp: 2026-03-15T21:15:25.076Z
+Commit: Patch API endpoint error handling
